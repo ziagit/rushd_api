e7befb4a4516c59df207b3de6efd8071b2f35c1f
ziagit <zia.csco@gmail.com>
2024-03-03 14:08:38 +0430

    changes in migrations

--- database/migrations/2023_12_26_126050_create_exams_table.php
+++ database/migrations/2023_12_26_126050_create_exams_table.php
@@ -16,12 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->dateTime('date')->nullable();
-            $table->unsignedBigInteger('type_id');
             $table->timestamps();
             $table->unsignedBigInteger('school_id');
             $table->foreign('school_id')->references('id')->on('schools')->onDelete('cascade');
-        
-            $table->foreign('type_id')->references('id')->on('exam_types');
         });
     }
 
